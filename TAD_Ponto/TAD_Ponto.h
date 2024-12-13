d72b7a30665fdd7810eebc55f21920be7e9fc507
profraimundovalter <valter.costa@ifce.edu.br>
2024-12-13 17:44:40 -0300

    TAD Ponto inicial

---
+++ TAD_Ponto/TAD_Ponto.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+struct ponto{
+    float x;
+    float y;
+};
+
+typedef struct ponto Ponto;
+typedef struct ponto * PPonto;
+
+PPonto ponto_cria(float x, float y);
+
+#include "TAD_Ponto.c"
